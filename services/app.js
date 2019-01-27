24d3cf37e2d5830389c3e866aee8ae3ef5399f52
Rushil N <rnagarsheth-intern@bbmtek.com>
2019-01-27 10:32:11 -0500

    Added firebase and face api logic

--- services/app.js
+++ services/app.js
@@ -1,17 +1,11 @@
+// ---- SETUP ----
+
 const express = require("express");
 var firebase = require("firebase");
-// var storage = require("firebase/storage");
-// import firebase from "firebase";
+const request = require('request');
 
-/*
-
-
-* - To Do: 
-
-1. Get face ID from Firebase Database
-2. Send each face ID
-
-*/
+var matchFound = false;
+var threshold = 0.6;
 require("dotenv").config();
 
 const app = express();
@@ -22,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var config = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "aleksecurity.firebaseapp.com",
   databaseURL: "https://aleksecurity.firebaseio.com",
   projectId: "aleksecurity",
@@ -30,10 +24,7 @@ var config = {
   messagingSenderId: "840580222472"
 };
 
-function verifyTwoIds(id1,id2){
-  console.log("Comparing "+id1+" and "+id2);
-}
-
+// ------- MAIN CODE ---------
 
 firebase.initializeApp(config);
 
@@ -42,34 +33,120 @@ var db = firebase.database();
 
 var dbRefUsers = db.ref("/users");//.child("users"); // Reference to current users list
 
-db.ref("/current").on("value", snapshot => {
-  if (snapshot.val()) {
-    console.log("\n\nThere is a new user at door! " + snapshot.val()["currentUserAtDoor"]);
-    var currentUser = snapshot.val()["currentUserAtDoor"];
-    return dbRefUsers.once('value').then(function(userSnapshot) {
-      var userData = userSnapshot.val();
-
-      console.log("The user data is : ");
-      console.log(userData);
-    
-      for(var key in userData){
-
-        console.log("The key is: "+key+" and the value is: "+userData[key]);
-        verifyTwoIds(currentUser,userData[key]);
 
 
+
+function verifyTwoIds(id2,id1,name){
+
+
+  console.log("Comparing "+id1+" WITH ---> "+id2);
+  // Replace <Subscription Key> with your valid subscription key.
+  const subscriptionKey = process.env.AZURE_API_KEY;
+
+  // You must use the same location in your REST call as you used to get your
+  // subscription keys. For example, if you got your subscription keys from
+  // westus, replace "westcentralus" in the URL below with "westus".
+  const uriBase = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/verify';
+
+  const options = {
+      uri: uriBase,
+      body: '{"faceId1": ' + '"' + id1 + '","faceId2": '+ '"'+id2+'"'+'}',
+      headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key' : subscriptionKey
+      }
+  };
+
+  request.post(options, (error, response, body) => {
+    if (error) {
+      console.log('Error: ', error);
+      return;
+    }
+    let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+
+    var resp = JSON.parse(body);
+
+    if ("error" in resp){
+      console.log("--------- ERROR -----------");
+      var errorResp = resp["error"];
+      console.log("ERROR MESSAGE: "+errorResp["message"]);
+    }
+    else{
+      var isIdentical = resp["isIdentical"];
+      var confidence = resp["confidence"];
+
+      isIdentical = confidence>threshold;
+      console.log("\nSuccess! : "+String(confidence)+" ---- "+String(isIdentical));
+      if (isIdentical){
+        console.log("ITS A MATCH ----- "+name);
+        db.ref("/isMatch").set({
+          name:name,
+          value: true
+        });
 
       }
+      else {
+        console.log("Did not match with "+name);
+        db.ref("/isMatch").update({
+          value: false
+        });
+      }
+      console.log("Is identical ? "+String(isIdentical));
+      console.log("Confidence amount: "+String(confidence));
+      matchFound = isIdentical;
+      return isIdentical;
+    }
+  });
+}
 
+db.ref("/authenticate").on("value", snapshot =>{
+  
+  var authenticate = snapshot.val()["value"];
+  var name = snapshot.val()["name"];
 
+  if (authenticate){
+    console.log("Authentication request received from Alexa FOR NAME = "+name);
 
-      
-    
-    
-    
-    })
+    var faceId = "initial";
 
+    db.ref("/current").once('value').then(function(currentSnapshot) {
+      faceId = currentSnapshot.val()["currentUserAtDoor"];
+      console.log("Face ID for "+name+" is : "+faceId);
+      var dict = {};
+      dict[name] = faceId;
+
+      db.ref("/users").update(dict);
+
+      db.ref("/authenticate").update({
+        value:false
+      });
+    });  
   }
+
+});
+
+
+db.ref("/current").on("value", snapshot => {
+  console.log("\n\nThere is a new user at door! " + snapshot.val()["currentUserAtDoor"]);
+  var currentUser = snapshot.val()["currentUserAtDoor"];
+  return dbRefUsers.once('value').then(function(userSnapshot) {
+    var userData = userSnapshot.val();
+
+    console.log("The user data is : ");
+    console.log(userData);
+  
+    for(var key in userData){
+      console.log("The key is: "+key+" and the value is: "+userData[key]);
+      var isMatch = verifyTwoIds(currentUser,userData[key],key);
+      if(isMatch){
+        console.log("Match found! This guy is "+key);
+        console.log("BREAKING! ")
+        break;
+      }
+    } 
+  
+  })
+
 });
 
 
